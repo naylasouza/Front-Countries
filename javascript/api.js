32b44e9fc94555fe1d6cb156f54d7c18c554899b
nayla souza ribeiro <naylasouzariibeiro@gmail.com>
2020-09-29 05:41:30 -0300

    Adicionando conteudo brasil vindo da api

--- javascript/api.js
+++ javascript/api.js
@@ -7,6 +7,27 @@ const brasil = () => {
             const dadosBrasil = dadosJson[0];
             const bandeira = document.getElementById("bandeira");
             bandeira.setAttribute("src", dadosBrasil.flag)
+            
+            const nomePais = `${dadosBrasil.name} - (${dadosBrasil.alpha3Code})`
+            const paragrafoNomePais = document.getElementById("nomepais");
+            paragrafoNomePais.textContent = nomePais;
+            
+            const dominio = dadosBrasil.topLevelDomain;
+            const dominioElemento = document.getElementById("dominio");
+            dominioElemento.textContent = dominio;
+        
+            const sigla = `${dadosBrasil.alpha2Code} e ${dadosBrasil.alpha3Code}`;
+            const siglaElemento = document.getElementById("sigla");
+            siglaElemento.textContent = sigla;
+
+            const ddi = dadosBrasil.callingCodes;
+            const ddiElemento = document.getElementById("ddi");
+            ddiElemento.textContent = ddi;
+
+            const capital = dadosBrasil.capital;
+            const capitalElemento = document.getElementById("capital");
+            capitalElemento.textContent = capital;
+
         })
 }
 brasil();
