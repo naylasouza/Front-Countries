9e0bec3296f94f2c1b56bcaee6aac382c23f2b58
nayla souza ribeiro <naylasouzariibeiro@gmail.com>
2020-09-29 22:51:22 -0300

    Adicionando descricoes para o botao regional

--- javascript/regional.js
+++ javascript/regional.js
@@ -2,7 +2,7 @@ const regional = (nomeBlocoRegional) =>{
     return fetch(`${apiUrl}/regionalbloc/${nomeBlocoRegional}`)
     .then((response) => { return response.json()})
     .then((dadosBloco) => {
-        const containerBandeira = document.getElementById("paisesId")
+        const containerpaises = document.getElementById("paisesId")
         const htmlCards = dadosBloco.reduce((acumulador, pais) => {
 
             acumulador += `
@@ -18,7 +18,7 @@ const regional = (nomeBlocoRegional) =>{
             `
             return acumulador;
         },'');
-        containerBandeira.innerHTML = htmlCards;
+        containerpaises.innerHTML = htmlCards;
     })
 }
 
@@ -30,9 +30,3 @@ const blocosRegionais = document.querySelectorAll('.regional');
          regional(codigodobloco)
      })
  })
-
-//foreach = loop ..pra cada// tipo map
-//linha 13 = target é o elemento dono do evento que ta acontecendo// pegando o texto do elemento e tolowe.. 
-//converte tudo pra minusculo.
-//13 = cod é a varial que ta com o valor 
-//linha 1 e 14 sao a mesma coisa so que uma define e outra chama a funcao
